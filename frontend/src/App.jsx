a0b7a66f540478070a1f23e3dd51f5d2973d5b51
VinodKumarReddy14 <imvinodreddy14@gmail.com>
2025-10-21 14:09:12 +0530

    setup components, pages, axios and zustand config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import SettingPage from "./pages/SettingPage";
+import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
 
 const App = () => {
-  return <div className="text-red-500">hELLO</div>;
+  const { authUser, checkAuth } = useAuthStore;
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ frontend/src/store/useAuthStore.jsx
@@ -0,0 +1,22 @@
+import { axiosInstance } from "../lib/axios";
+import {create} from zustand;
+
+export const useAuthStore = create((set)=>({
+    authUser : null,
+    isCheckingAuth : true,
+    isSigningUp : false,
+    isLoggingIn : false,
+    isUpdatingProfile : false,
+    checkAuth : async () => {
+        try {
+            const res = await axiosInstance.get("/auth/check");
+            set({authUser : res.data});
+        } catch (error) {
+            console.log(`error in checkAuth, ${error.message}`);
+            set({authUser : false});  
+        } finally {
+            set({isCheckingAuth : false});
+        }
+        
+    }
+}));
